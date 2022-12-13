4ea1fd678cb452c4b8d2c69953813b8a80ad895e
NicoNicoPizza <47262000+NicoNicoPizza@users.noreply.github.com>
2022-12-13 09:45:32 +0100

    Add files via upload

---
+++ random_image.py
@@ -0,0 +1,35 @@
+#coding=UTF-8
+
+#Modules
+import tweepy
+import glob
+import random
+import os
+import shutil
+
+#Access
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit = True)
+
+#Post random image
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+def imgaleatoria(folder):
+    images = glob.glob(folder + "*")
+    image_open = images[random.randint(0,len(images))-1]
+    image_size = os.path.getsize(image_open)
+
+    api.update_with_media(image_open)
+
+    #Prevent duplicate images
+    #shutil.copy(image_open, "Copias/")
+    #os.remove(image_open)
+
+imgaleatoria("Images/")
